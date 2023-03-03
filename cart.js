aa21d963e7b8389202015d98a74a971964e82634
Mazen Elsayegh <mazen.elsayegh120298@gmail.com>
2023-03-03 21:40:17 +0200

    edit

--- cart.js
+++ cart.js
@@ -1,28 +1,17 @@
-// if(JSON.parse(localStorage.getItem("ProductDetails")) != null){
-//     let checkEmail = JSON.parse(localStorage.getItem("tempData")).email;
-//     console.log(checkEmail);
-
-// }
-
-// let addCartBtn = document.getElementById("addCartBtn");
-// addCartBtn.addEventListener("click", cartDetails);
-
 var addedToCart = document.getElementById("addedToCart");
 var noProducts = document.getElementById("noProducts");
 var addedProducts = document.getElementById("addedProducts");
 var cartIcon = document.getElementById("cartIconID");
 
 cartIcon.addEventListener("click", cartDetails);
-cartIcon.addEventListener("click", switchTurnOff);
 
 function cartDetails() {
   if (JSON.parse(localStorage.getItem("ProductDetails")) != null) {
     let data = JSON.parse(localStorage.getItem("ProductDetails"));
-    console.log(data);
-
+    // console.log(data);
     if (JSON.parse(localStorage.getItem("ProductDetails")) != null) {
       let checkEmail = JSON.parse(localStorage.getItem("tempData")).email;
-      console.log(checkEmail);
+      // console.log(checkEmail);
       addedProducts.innerHTML = "";
       for (let i = 0; i < data.length; i++) {
         if (data[i].email == checkEmail) {
@@ -50,36 +39,3 @@ function cartDetails() {
     }
   }
 }
-// cartDetails();
-
-// function switchTurnOff() {
-//   let addedToCart = document.getElementById("addedToCart");
-//   let noProducts = document.getElementById("noProducts");
-//   let data = JSON.parse(localStorage.getItem("ProductDetails"));
-//   let flag=0;
-//   for (let i = 0; i < data.length; i++) {
-//     if (data[i].email == checkEmail) {
-//       flag=1;
-//     }
-//   }
-//   if(flag==0)
-//   addedToCart.classList.add("turnOff");
-//   noProducts.classList.remove("turnOff");
-//   addedToCart.classList.remove("addedToCartOn");
-// }
-
-// let flag = 0;
-// for (let i = 0; i < data.length; i++) {
-//   if (data[i].email == checkEmail) {
-//     addedToCart.classList.remove("turnOff");
-//     addedToCart.classList.add("addedToCartOn");
-//     noProducts.classList.add("turnOff");
-//     flag = 1;
-//     break;
-//   }
-// }
-// if (flag == 0) {
-//   addedToCart.classList.add("turnOff");
-//   noProducts.classList.remove("turnOff");
-//   addedToCart.classList.remove("addedToCartOn");
-// } else {}
