0cf8207099a8031ce94e97a488f7be3233ba1976
Mazen Elsayegh <mazen.elsayegh120298@gmail.com>
2023-03-03 20:36:24 +0200

    cart function

--- cart.js
+++ cart.js
@@ -10,11 +10,30 @@
 var addedToCart = document.getElementById("addedToCart");
 var noProducts = document.getElementById("noProducts");
 var addedProducts = document.getElementById("addedProducts");
+var cartIcon = document.getElementById("cartIconID");
+
+cartIcon.addEventListener("click", cartDetails);
+cartIcon.addEventListener("click", switchTurnOff);
 
 function cartDetails() {
   if (JSON.parse(localStorage.getItem("ProductDetails")) != null) {
     let data = JSON.parse(localStorage.getItem("ProductDetails"));
     console.log(data);
+    // let flag = 0;
+    // for (let i = 0; i < data.length; i++) {
+    //   if (data[i].email == checkEmail) {
+    //     addedToCart.classList.remove("turnOff");
+    //     addedToCart.classList.add("addedToCartOn");
+    //     noProducts.classList.add("turnOff");
+    //     flag = 1;
+    //     break;
+    //   }
+    // }
+    // if (flag == 0) {
+    //   addedToCart.classList.add("turnOff");
+    //   noProducts.classList.remove("turnOff");
+    //   addedToCart.classList.remove("addedToCartOn");
+    // } else {}
     if (JSON.parse(localStorage.getItem("ProductDetails")) != null) {
       let checkEmail = JSON.parse(localStorage.getItem("tempData")).email;
       console.log(checkEmail);
@@ -22,6 +41,7 @@ function cartDetails() {
       addedToCart.classList.add("addedToCartOn");
 
       noProducts.classList.add("turnOff");
+      addedProducts.innerHTML = "";
       for (let i = 0; i < data.length; i++) {
         if (data[i].email == checkEmail) {
           addedProducts.innerHTML += `
@@ -40,12 +60,25 @@ function cartDetails() {
                  </div>
                 
                 `;
-
-          console.log("flag");
-          // omarid.setAttribute(data.img)
         }
       }
     }
   }
 }
-cartDetails();
+// cartDetails();
+
+// function switchTurnOff() {
+//   let addedToCart = document.getElementById("addedToCart");
+//   let noProducts = document.getElementById("noProducts");
+//   let data = JSON.parse(localStorage.getItem("ProductDetails"));
+//   let flag=0;
+//   for (let i = 0; i < data.length; i++) {
+//     if (data[i].email == checkEmail) {
+//       flag=1;
+//     }
+//   }
+//   if(flag==0)
+//   addedToCart.classList.add("turnOff");
+//   noProducts.classList.remove("turnOff");
+//   addedToCart.classList.remove("addedToCartOn");
+// }
