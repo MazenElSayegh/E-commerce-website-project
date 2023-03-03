45b0bc1fc94eab3532534b04a8a2bd529dd072ec
Omar Mohamed <omarmohamedradi97@gmail.com>
2023-03-03 18:08:39 +0200

    Adding footer

--- cart.js
+++ cart.js
@@ -1,4 +1,3 @@
-
 // if(JSON.parse(localStorage.getItem("ProductDetails")) != null){
 //     let checkEmail = JSON.parse(localStorage.getItem("tempData")).email;
 //     console.log(checkEmail);
@@ -12,43 +11,41 @@ var addedToCart = document.getElementById("addedToCart");
 var noProducts = document.getElementById("noProducts");
 var addedProducts = document.getElementById("addedProducts");
 
-function cartDetails(){
-if (JSON.parse(localStorage.getItem("ProductDetails")) != null){
+function cartDetails() {
+  if (JSON.parse(localStorage.getItem("ProductDetails")) != null) {
     let data = JSON.parse(localStorage.getItem("ProductDetails"));
     console.log(data);
-    if(JSON.parse(localStorage.getItem("ProductDetails")) != null){
-        let checkEmail = JSON.parse(localStorage.getItem("tempData")).email;
-        console.log(checkEmail);
-        addedToCart.classList.remove("turnOff");
-        addedToCart.classList.add("addedToCartOn");
+    if (JSON.parse(localStorage.getItem("ProductDetails")) != null) {
+      let checkEmail = JSON.parse(localStorage.getItem("tempData")).email;
+      console.log(checkEmail);
+      addedToCart.classList.remove("turnOff");
+      addedToCart.classList.add("addedToCartOn");
 
-        noProducts.classList.add("turnOff");
-        for(let i =0; i<data.length;i++){
-            if(data[i].email==checkEmail){
-                
-
-                addedProducts.innerHTML +=`
+      noProducts.classList.add("turnOff");
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].email == checkEmail) {
+          addedProducts.innerHTML += `
                 <div id="cartItemContainer">
 
                         <img id="itemImage" src="${data[i].img}">
                         <span id="itemName">Name: ${data[i].title}</span>
                         <span id="itemPrice">Price: ${data[i].price}</span>
-                        <span id="itemQuantity">Quantity: ${data[i].quantity}</span>
-                        <span id="itemTP">Total Price: ${data[i].quantity*data[i].price}</span>
+                        <span id="itemQuantity">Quantity: ${
+                          data[i].quantity
+                        }</span>
+                        <span id="itemTP">Total Price: ${
+                          data[i].quantity * data[i].price
+                        }</span>
 
                  </div>
                 
-                `
-                
-                console.log("flag");
-                // omarid.setAttribute(data.img)
-            
-            }
+                `;
 
-
-        }   
-    
+          console.log("flag");
+          // omarid.setAttribute(data.img)
+        }
+      }
     }
-}
+  }
 }
 cartDetails();
