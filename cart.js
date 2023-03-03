396b4792184a6559d0e5a9bcd7410cc81bfdf35d
Mazen Elsayegh <mazen.elsayegh120298@gmail.com>
2023-03-03 21:03:55 +0200

    cart function

--- cart.js
+++ cart.js
@@ -19,31 +19,16 @@ function cartDetails() {
   if (JSON.parse(localStorage.getItem("ProductDetails")) != null) {
     let data = JSON.parse(localStorage.getItem("ProductDetails"));
     console.log(data);
-    // let flag = 0;
-    // for (let i = 0; i < data.length; i++) {
-    //   if (data[i].email == checkEmail) {
-    //     addedToCart.classList.remove("turnOff");
-    //     addedToCart.classList.add("addedToCartOn");
-    //     noProducts.classList.add("turnOff");
-    //     flag = 1;
-    //     break;
-    //   }
-    // }
-    // if (flag == 0) {
-    //   addedToCart.classList.add("turnOff");
-    //   noProducts.classList.remove("turnOff");
-    //   addedToCart.classList.remove("addedToCartOn");
-    // } else {}
+
     if (JSON.parse(localStorage.getItem("ProductDetails")) != null) {
       let checkEmail = JSON.parse(localStorage.getItem("tempData")).email;
       console.log(checkEmail);
-      addedToCart.classList.remove("turnOff");
-      addedToCart.classList.add("addedToCartOn");
-
-      noProducts.classList.add("turnOff");
       addedProducts.innerHTML = "";
       for (let i = 0; i < data.length; i++) {
         if (data[i].email == checkEmail) {
+          addedToCart.classList.remove("turnOff");
+          addedToCart.classList.add("addedToCartOn");
+          noProducts.classList.add("turnOff");
           addedProducts.innerHTML += `
                 <div id="cartItemContainer">
 
@@ -82,3 +67,19 @@ function cartDetails() {
 //   noProducts.classList.remove("turnOff");
 //   addedToCart.classList.remove("addedToCartOn");
 // }
+
+// let flag = 0;
+// for (let i = 0; i < data.length; i++) {
+//   if (data[i].email == checkEmail) {
+//     addedToCart.classList.remove("turnOff");
+//     addedToCart.classList.add("addedToCartOn");
+//     noProducts.classList.add("turnOff");
+//     flag = 1;
+//     break;
+//   }
+// }
+// if (flag == 0) {
+//   addedToCart.classList.add("turnOff");
+//   noProducts.classList.remove("turnOff");
+//   addedToCart.classList.remove("addedToCartOn");
+// } else {}
